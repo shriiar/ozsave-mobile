c84480b95f908f262ac3e65ab467ad420631beb1
shriiar <fahimshriiar@gmail.com>
2026-06-17 20:27:04 +1000

    increase maxHeight of AddCostModal to improve layout

--- src/modules/cost/AddCostModal.tsx
+++ src/modules/cost/AddCostModal.tsx
@@ -842,7 +842,7 @@ const styles = StyleSheet.create({
         borderRadius: 16,
         overflow: "hidden",
         // borderWidth: StyleSheet.hairlineWidth,
-        maxHeight: "40%",
+        maxHeight: "70%",
         width: "100%",
     },
 
